6c6b2b0efd8d3c39f55f321b80eafe94ce456376
Sven Hagemann <sven@savagetiger.org>
2022-12-27 20:49:54 +0100

    Load entities on level load
    Instead of each call, load the entities collection when a level loads.

--- entities.cpp
+++ entities.cpp
@@ -1,37 +1,38 @@
 #include "entities.h"
 #include "entity.h"
 
-struct entityProperties {
-    float size;
-    int x;
-    int y;
-    float top;
-};
-
 entity G_ENTITY;
+
+entities::entities(map* map) {
+    this->loadEntities(map);
+}
 
 void entities::render(player *player, textures* textures, wall** walls)
 {
-    entityProperties entities[1];
+    for (auto& entity : entityMemory) {
+        if (entity.size == 0) {
+            break;
+        }
 
-    entityProperties gun;
-
-    gun.size = .3;
-    gun.x = 90;
-    gun.y = 90;
-    gun.top = .7;
-
-    entities[0] = gun;
-
-    for (int i = 0; i < sizeof(entities); i++) {
         G_ENTITY.render(
-            entities[i].size,
-            entities[i].x,
-            entities[i].y,
-            entities[i].top,
+            entity.size,
+            (entity.tileLeft * (TILE_SIZE) - (TILE_SIZE / 2)),
+            (entity.tileTop * (TILE_SIZE) - (TILE_SIZE / 2)),
+            entity.distanceFromCieling,
             player,
             textures,
             walls
         );
     }
+}
+
+void entities::loadEntities(map* map) {
+    entityProperties gun;
+
+    gun.size = .3;
+    gun.tileTop = 5;
+    gun.tileLeft = 2;
+    gun.distanceFromCieling = .7;
+
+    entityMemory[0] = gun;
 }
--- entities.h
+++ entities.h
@@ -5,9 +5,23 @@
 #include "wall.h"
 #include "textures.h"
 
+struct entityProperties {
+    float size;
+    int tileTop;
+    int tileLeft;
+    float distanceFromCieling;
+};
+
 class entities {
 
+private:
+    entityProperties entityMemory[128];
+
+    void loadEntities(map* map);
+
 public:
+    entities(map *map);
+
     void render(player *player, textures* textures, wall** walls);
 
 };
